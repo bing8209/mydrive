d79bfd8e608830e5d49336c8bab837251c96edf3
bing8209 <11@cxb.qzz.io>
2026-06-16 17:37:17 +0800

    Update App.xaml.cs

--- LuckyDrive/App.xaml.cs
+++ LuckyDrive/App.xaml.cs
@@ -1,18 +1,6 @@
-using System;
-using System.Windows;
-
-namespace LuckyDrive
-{
-    /// <summary>
-    /// App.xaml 的交互逻辑
-    /// </summary>
-    public partial class App : Application
-    {
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            base.OnStartup(e);
-            
-            // 这里可以处理一些程序启动时的初始化逻辑（例如检查管理员权限、单实例运行等）
-        }
-    }
-}
+<Application x:Class="LuckyDrive.App"
+             xmlns="http://schemas.microsoft.com/winfx/2000/xaml/presentation"
+             xmlns:x="http://schemas.microsoft.com/winfx/2000/xaml">
+    <Application.Resources>
+         </Application.Resources>
+</Application>
