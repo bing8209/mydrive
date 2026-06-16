197b7a5abd40dda160e31cbf4303c3a2368fb8d3
bing8209 <11@cxb.qzz.io>
2026-06-16 17:40:11 +0800

    Update App.xaml.cs

--- LuckyDrive/App.xaml.cs
+++ LuckyDrive/App.xaml.cs
@@ -1,6 +1,17 @@
-<Application x:Class="LuckyDrive.App"
-             xmlns="http://schemas.microsoft.com/winfx/2000/xaml/presentation"
-             xmlns:x="http://schemas.microsoft.com/winfx/2000/xaml">
-    <Application.Resources>
-         </Application.Resources>
-</Application>
+using System;
+using System.Windows;
+
+namespace LuckyDrive
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            
+            // 🚀 核心关键：手动在内存里强行实例并展示我们纯 C# 的 MainWindow 窗口
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
+    }
+}
